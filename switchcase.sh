1de7ba8a51c523dadd9fd85703d5b2c96f8a3255
kelvkedyson <kelvinkedysonzacharia@gmail.com>
2020-09-10 21:49:38 +0300

    Added descriptive comments

--- switchcase.sh
+++ switchcase.sh
@@ -5,19 +5,20 @@
 #
  echo "Enter a number between 1-5: "
 
- read number
+ read number #taking the user's input
 
  case $number in
  	1) echo "number is $number"
-		;;
+		;; #break
 	2) echo "number is $number"
-		;;
+		;; #break
 	3) echo "number is $number"
-		;;
+		;; #break
 	4) echo "number is $number"
-		;;
+		;; #break
 	5) echo "number is $number"
-		;;
+		;; #break
+	#checking for other values (default)
 	*) if [ $number -le 0 ] 
 		then
 		#statements
@@ -27,4 +28,4 @@
 		#statements
 		echo "$number is greater than 5: OUT OF RANGE"
 	fi 
-esac
+esac #end switch
