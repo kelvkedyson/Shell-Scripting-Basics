7edf0608e823de5a697ea92b74afe9647abf5ed7
kelvkedyson <kelvinkedysonzacharia@gmail.com>
2020-09-10 22:05:59 +0300

    Modified

--- forloop.sh
+++ forloop.sh
@@ -8,3 +8,20 @@ for i in {1..5}
 do
 	echo "$i : Hello World"
 done
+
+echo ''
+echo "Alternatively"
+#alternatively, NB: the order of the numeber sequence does not matter, it is not a must to be consecutive
+for i in 3 4 5 6
+do
+	echo "$i : Bash is sweet"
+done
+
+echo ''
+echo "Printing all the files in the current directory"
+#printing all the files in a directory
+
+for fileName in *
+do
+	echo "file name : $fileName"
+done
