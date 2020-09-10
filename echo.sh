d22c60943fe36cd96a49fe32415267460b2afdb5
kelvkedyson <kelvinkedysonzacharia@gmail.com>
2020-09-10 08:57:55 +0300

    Added alternative way to print a super user

--- echo.sh
+++ echo.sh
@@ -10,5 +10,8 @@ echo "List *"
 #printing the name of the user of the computer
 echo $USER
 
+#or it could be written as
+echo "$USER"
+
 # using * , all the files in the current directory will be echo(ed)
 echo "List : " *
