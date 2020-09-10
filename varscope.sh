33dd6da9487f607b302e6e359dc6e28222927d6b
kelvkedyson <kelvinkedysonzacharia@gmail.com>
2020-09-10 13:15:14 +0300

    Added descriptive comments

--- varscope.sh
+++ varscope.sh
@@ -6,3 +6,6 @@ echo "$name"
 #when running this script, type export name="[your preffered name]"
 #using export keyword makes the variable name public in all the sessions and then override its literal
 #for the second session to take the previous results, in your terminal write : . ./[name of your .sh file]
+#
+#export means the variable is shared between different sessions
+#example run cat ~/.profile to view all the variables that are public
