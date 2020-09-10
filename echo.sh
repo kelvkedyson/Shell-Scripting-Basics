b104af4005b0451f8b2d99d02a2565fffc4d62ac
kelvkedyson <kelvinkedysonzacharia@gmail.com>
2020-09-10 08:46:01 +0300

    How to use echo

---
+++ echo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#different ways to echo
+echo "Hello World"
+
+echo Hello World
+
+echo "List *"
+
+# using * , all the files in the current directory will be echo(ed)
+echo "List : " *
