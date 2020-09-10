73d77cb841870ab15eb59e5dc648489396eecd22
kelvkedyson <kelvinkedysonzacharia@gmail.com>
2020-09-10 08:56:14 +0300

    Added code print/echo a super user of the computer

--- echo.sh
+++ echo.sh
@@ -7,5 +7,8 @@ echo Hello World
 
 echo "List *"
 
+#printing the name of the user of the computer
+echo $USER
+
 # using * , all the files in the current directory will be echo(ed)
 echo "List : " *
