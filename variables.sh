178ae24c070b66c2bbff736ba8091fff2bac258f
kelvkedyson <kelvinkedysonzacharia@gmail.com>
2020-09-10 09:09:30 +0300

    Working with variables

---
+++ variables.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#defining a variable, NB: NO SPACE BETWEEN A LITERAL, VARIABLE NAME AND THE ASSIGNMENT OPERATOR
+program=Telecommunications
+
+echo $program #this outputs the literal of varible program
+
+#WARNING: this does not print the value of the variable program
+echo program # this just print the word program
+
+echo '' #this prints only space
+
+name="Kedyson" #this is the best way to store a string in a variable
+echo $name
+
+echo ''
+
+#for integers
+integerNo=1
+echo $integerNo
+
+echo ''
+
+#floats
+floatNo=1.2
+echo $floatNo
+
+echo ''
+
+#double
+doubleNo=1.33
+echo $doubleNo
+echo ''
