5bb0257ff4da6c509262bf7ffdd67e3e2e36bebe
kelvkedyson <kelvinkedysonzacharia@gmail.com>
2020-09-11 14:25:14 +0300

    Added continue to skip number 5

--- brkcont.sh
+++ brkcont.sh
@@ -3,7 +3,13 @@
 #controlling loop using break and continue statements
 #
 
+echo "======= USING CONTINUE ======="
 for i in {1..10}
 do
+	if [[ "$i" -eq 5 ]] #This skips number 5 and continue up to ten
+	then
+		#statements
+		continue
+	fi
 	echo "$i"
 done
