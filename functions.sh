261c9985e306ca710a312bd957c4d1beb7ba8b4f
kelvkedyson <kelvinkedysonzacharia@gmail.com>
2020-09-11 14:38:52 +0300

    Added comments

--- functions.sh
+++ functions.sh
@@ -3,9 +3,10 @@
 #functions
 #
 
+# $0 means the caller ($1-$9 passing the variables)
 #creating and defining a function
 printUserName(){
-	echo "$USER"
+	echo "$USER called by : $0"
 }
 
 #calling the function
