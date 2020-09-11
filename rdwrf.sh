01f24df70e607a177ee34d201ea061337c52c8f9
kelvkedyson <kelvinkedysonzacharia@gmail.com>
2020-09-11 15:23:01 +0300

    Modified

--- rdwrf.sh
+++ rdwrf.sh
@@ -6,6 +6,7 @@
 #writing contents to files
 
 echo "Hello, Kelvin" > sample.txt
+echo "You are a Telecom engineer" > sample.txt #this overwrites the content of the file
 
 #getting the contents from the sample.txt file using cat command
 cat sample.txt
