298d00d6c2454377ebf4dfa8b0a6d424f10f7de2
kelvkedyson <kelvinkedysonzacharia@gmail.com>
2020-09-11 15:26:01 +0300

    Modified code to read line by line using while loop

--- rdwrf.sh
+++ rdwrf.sh
@@ -11,3 +11,11 @@ echo "You are a Tanzanian" >> sample.txt #this add more line to the file
 
 #getting the contents from the sample.txt file using cat command
 cat sample.txt
+echo ''
+
+#using while loop to read line by line from the file sample.txt
+while read line
+do
+	#statements
+	echo "$line"
+done < sample.txt
