c0a1e99ff91e1afaf4b61178886f2940be8b2257
kelvkedyson <kelvinkedysonzacharia@gmail.com>
2020-09-11 15:21:36 +0300

    Reading and Writing from files

---
+++ rdwrf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#Reading and writing to files
+#
+
+#writing contents to files
+
+echo "Hello, Kelvin" > sample.txt
+
+#getting the contents from the sample.txt file using cat command
+cat sample.txt
