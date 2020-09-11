33a1a158c544b697aa794614cf05bec2a476d680
kelvkedyson <kelvinkedysonzacharia@gmail.com>
2020-09-11 14:28:14 +0300

    Added break statement to count only from 1 to 4, the loop stops when i is 5

--- brkcont.sh
+++ brkcont.sh
@@ -13,3 +13,16 @@ do
 	fi
 	echo "$i"
 done
+
+echo ''
+
+echo "=======  USING BREAK ======="
+for i in {1..10}
+do
+	if [[ "$i" -eq 5 ]] #the loop stops as i is equal to 5 (the loop counts from 1 to 4)
+	then
+		#statements
+		break
+	fi
+	echo "$i"
+done
