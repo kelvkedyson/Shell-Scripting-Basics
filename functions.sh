1bfc2d93f894dad25266ce462b6bba32f35d6d20
kelvkedyson <kelvinkedysonzacharia@gmail.com>
2020-09-11 14:42:33 +0300

    Added function thataccept one argument

--- functions.sh
+++ functions.sh
@@ -3,7 +3,7 @@
 #functions
 #
 
-# $0 means the caller ($1-$9 passing the variables)
+# $0 means the caller ($1-$x passing the variables)
 #creating and defining a function
 printUserName(){
 	echo "$USER called by : $0"
@@ -11,3 +11,11 @@ printUserName(){
 
 #calling the function
 printUserName
+
+#function that pass an argument when called
+showParam(){
+	echo "Name : $1" #this means takes the first argument
+}
+
+#calling the function and pass argument Kedyson
+showParam Kedyson
