fb4f68b124866bf8dfb0ccf71d4cb69a66ef8408
kelvkedyson <kelvinkedysonzacharia@gmail.com>
2020-09-11 15:23:56 +0300

    Modified

--- rdwrf.sh
+++ rdwrf.sh
@@ -7,6 +7,7 @@
 
 echo "Hello, Kelvin" > sample.txt
 echo "You are a Telecom engineer" > sample.txt #this overwrites the content of the file
+echo "You are a Tanzanian" >> sample.txt #this add more line to the file
 
 #getting the contents from the sample.txt file using cat command
 cat sample.txt
